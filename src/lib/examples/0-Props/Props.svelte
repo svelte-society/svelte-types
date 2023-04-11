76e6468b92d52c3e2d76539d089b6223f27b6281
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-04-11 13:16:42 +0200

    fix example

--- src/lib/examples/0-Props/Props.svelte
+++ src/lib/examples/0-Props/Props.svelte
@@ -2,9 +2,4 @@
 	export let count: number;
 </script>
 
-padisyqwoince ulesjnf la on:
-
-{#each items as item}
-	<!-- content here -->asdakjhlkljashdlksajdhaslkjdhsalkdjashdlkjsahdaslkjdhasdlkjashdlkasjh
-{/each}
 {count}
