68a6f57306100e67494cdbf477c4c88a65893bf8
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-04-11 14:42:08 +0200

    add two more examples

---
+++ src/lib/examples/1-Optional-Props/OptionalProps.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let firstName: string;
+	export let lastName: string;
+
+	// Inferred
+	$: greeting = 'Hello ' + firstName;
+
+	// Type it explicitly by creating a var like so 👇
+	let fancyGreeting: string;
+	$: fancyGreeting = 'Good evening ' + lastName;
+</script>
+
+{greeting}
+
+{fancyGreeting}
